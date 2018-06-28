3c5849f57b77e3188e072bf681206436f31eae69
Kristijan Petrovski <petrovski.k@gmail.com>
2018-06-28 14:35:19 +0200

    added normalize port

--- server.js
+++ server.js
@@ -2,13 +2,40 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const { scraper } = require("./scraper");
+
+const PORT = 4000;
+/**
+ * Get port from environment and store in Express.
+ */
+
+var port = normalizePort(process.env.PORT || PORT);
+
 app.use(bodyParser.json());
 
 app.post("/scrape", async (req, res) => {
   const { query, vertical, location, scriptUrl } = req.body;
   let link = `https://www.bing.com/search?q=${query}&qs=n&first=0`;
   let results = await scraper(link, [], location, vertical, 10, scriptUrl);
-  res.send({ msg: "success", link});
+  res.send({ msg: "success", link });
 });
 
 app.listen(8080, () => console.log("Example app listening on port 8080!"));
+
+/**
+ * Normalize a port into a number, string, or false.
+ */
+function normalizePort(val) {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
