33b7a7beed44212ecb90036a998622856d04b1e0
unknown <mario_boskovski@hotmail.com>
2018-08-22 15:54:22 +0200

    commit

--- scrapeGoogle.js
+++ scrapeGoogle.js
@@ -6,19 +6,21 @@ const findCompany = companyNameQuery => {
     companyNameQuery
   )}`;
   console.log(url);
-  return axiosProxyRequest(url).then(resp => {
-    let html = resp.data;
-    // console.log(html)
-    let data = scrapeIt.scrapeHTML(html, {
-      address: ".A1t5ne",
-      website: {
-        selector: ".s",
-        listItem: "cite"
-      }
-    });
-   // console.log(data);
-    return [data.address || "", data.website[0] || ""];
-  });
+  return axiosProxyRequest(url)
+    .then(resp => {
+      let html = resp.data;
+      // console.log(html)
+      let data = scrapeIt.scrapeHTML(html, {
+        address: ".A1t5ne",
+        website: {
+          selector: ".s",
+          listItem: "cite"
+        }
+      });
+      // console.log(data);
+      return [data.address || "", data.website[0] || ""];
+    })
+    .catch(err => ["", ""]);
 };
 
 module.exports.findCompanyGoogle = findCompany;
